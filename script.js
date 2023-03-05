264376eeb42f2587148252658ed2d0d392ed2f94
Mathr81 <112965202+Mathr81@users.noreply.github.com>
2023-03-05 11:27:12 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,41 +1,78 @@
-// Code pour détecter la manette et mettre à jour l'état des boutons et sticks
-var gamepad = null;
+const buttons = [
+  "button-a",
+  "button-b",
+  "button-x",
+  "button-y",
+  "button-l",
+  "button-r",
+  "button-zl",
+  "button-zr",
+  "button-minus",
+  "button-plus",
+  "button-home",
+  "button-capture",
+  "dpad-up",
+  "dpad-down",
+  "dpad-left",
+  "dpad-right",
+  "button-stick-left",
+  "button-stick-right"
+];
+
+const sticks = [
+  { id: "stick-left", xId: "stick-left-x", yId: "stick-left-y" },
+  { id: "stick-right", xId: "stick-right-x", yId: "stick-right-y" }
+];
+
+let gamepad;
+
+function handleGamepad() {
+  if (!gamepad) {
+    return;
+  }
+
+  // Handle buttons
+  buttons.forEach((buttonId) => {
+    const button = document.getElementById(buttonId);
+    if (gamepad.buttons[buttons.indexOf(buttonId)].pressed) {
+      button.classList.add("active");
+    } else {
+      button.classList.remove("active");
+    }
+  });
+
+  // Handle sticks
+  sticks.forEach((stick) => {
+    const x = gamepad.axes[sticks.indexOf(stick) * 2];
+    const y = gamepad.axes[sticks.indexOf(stick) * 2 + 1];
+    const stickElement = document.getElementById(stick.id);
+    const xElement = document.getElementById(stick.xId);
+    const yElement = document.getElementById(stick.yId);
+    stickElement.classList.remove("active");
+    xElement.innerHTML = x.toFixed(2);
+    yElement.innerHTML = y.toFixed(2);
+    if (Math.abs(x) > 0.2 || Math.abs(y) > 0.2) {
+      stickElement.classList.add("active");
+    }
+  });
+}
+
+function connectHandler(e) {
+  gamepad = e.gamepad;
+  console.log(`Gamepad connected: ${gamepad.id}`);
+}
+
+function disconnectHandler(e) {
+  console.log(`Gamepad disconnected: ${gamepad.id}`);
+  gamepad = null;
+}
+
+window.addEventListener("gamepadconnected", connectHandler);
+window.addEventListener("gamepaddisconnected", disconnectHandler);
 
 function update() {
   requestAnimationFrame(update);
-  if (gamepad == null) {
-    var gamepads = navigator.getGamepads();
-    for (var i = 0; i < gamepads.length; i++) {
-      if (gamepads[i] != null && gamepads[i].id === "Pro Controller (STANDARD GAMEPAD Vendor: 057e Product: 2009)") {
-        gamepad = gamepads[i];
-      }
-    }
-  } else {
-    gamepad = navigator.getGamepads()[gamepad.index];
-    if (gamepad != null) {
-      document.getElementById("button-0").innerHTML = gamepad.buttons[0].value;
-      document.getElementById("button-1").innerHTML = gamepad.buttons[1].value;
-      document.getElementById("button-2").innerHTML = gamepad.buttons[2].value;
-      document.getElementById("button-3").innerHTML = gamepad.buttons[3].value;
-      document.getElementById("button-4").innerHTML = gamepad.buttons[4].value;
-      document.getElementById("button-5").innerHTML = gamepad.buttons[5].value;
-      document.getElementById("button-6").innerHTML = gamepad.buttons[6].value;
-      document.getElementById("button-7").innerHTML = gamepad.buttons[7].value;
-      document.getElementById("button-8").innerHTML = gamepad.buttons[8].value;
-      document.getElementById("button-9").innerHTML = gamepad.buttons[9].value;
-      document.getElementById("button-10").innerHTML = gamepad.buttons[10].value;
-      document.getElementById("button-11").innerHTML = gamepad.buttons[11].value;
-      document.getElementById("button-12").innerHTML = gamepad.buttons[12].value;
-      document.getElementById("button-13").innerHTML = gamepad.buttons[13].value;
-      document.getElementById("button-14").innerHTML = gamepad.buttons[14].value;
-      document.getElementById("button-15").innerHTML = gamepad.buttons[15].value;
-      document.getElementById("button-16").innerHTML = gamepad.buttons[16].value;
-      document.getElementById("stick-left-x").innerHTML = gamepad.axes[0].toFixed(2);
-      document.getElementById("stick-left-y").innerHTML = gamepad.axes[1].toFixed(2);
-      document.getElementById("stick-right-x").innerHTML = gamepad.axes[2].toFixed(2);
-      document.getElementById("stick-right-y").innerHTML = gamepad.axes[3].toFixed(2);
-    }
-  }
+  handleGamepad();
 }
 
-requestAnimationFrame(update);
+update();
